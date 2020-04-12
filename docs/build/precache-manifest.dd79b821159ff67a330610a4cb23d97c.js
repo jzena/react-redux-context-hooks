2cd3b2c7b128ed57d361c3c2e157cc9f5dd265d7
Jose Encarnacion Zeña Zamora <jose.zena@PE-IT000184.local>
2020-04-12 10:23:01 -0500

    build generation

---
+++ docs/build/precache-manifest.dd79b821159ff67a330610a4cb23d97c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba00cde1425433cbe4bd304bdbeb109c",
+    "url": "/react-redux-context-hooks/index.html"
+  },
+  {
+    "revision": "cb6c3ecba23854d9c9a8",
+    "url": "/react-redux-context-hooks/static/css/main.5fdc6bc9.chunk.css"
+  },
+  {
+    "revision": "a6a80d077d46224b73d7",
+    "url": "/react-redux-context-hooks/static/js/2.01350426.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-redux-context-hooks/static/js/2.01350426.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb6c3ecba23854d9c9a8",
+    "url": "/react-redux-context-hooks/static/js/main.635c33d0.chunk.js"
+  },
+  {
+    "revision": "aa7bfdd661820d3ab595",
+    "url": "/react-redux-context-hooks/static/js/runtime-main.f76195d4.js"
+  }
+]);
