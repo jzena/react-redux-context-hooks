690239dbf7b695fd51c60337847f3026c314acea
jzena <jose.zena.zamora@gmail.com>
2020-05-01 11:44:38 -0500

    adding hg-pages

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import ProductsPage from './pages/Products';
 import CartPage from './pages/Cart';
 import './App.css';
 
+const repo = `/${window.location.pathname.split('/')[1]}`;
 class App extends Component {
   render() {
     return (
       <GlobalState>
-        <BrowserRouter>
+        <BrowserRouter basename={repo}>
           <Switch>
             <Route path="/" component={ProductsPage} exact />
             <Route path="/cart" component={CartPage} exact />
